9ef06221b017216154a8a6d169f6ad0ed7286857
Jared Collier <jaredcollier@Jareds-MacBook-Pro.local>
2023-01-13 16:18:31 -0700

    adds unit tests

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,19 @@
 const {shuffleArray} = require('./utils')
 
+let testArr1 = ['eenee', 'meenee', 'miney', 'moe']
+let testArr2 = []
+let testArr3 = [1,2,3,3,4,5,6]
+
 describe('shuffleArray should', () => {
-    // CODE HERE
+    test('return an array', () => {
+        expect(Array.isArray(shuffleArray(testArr1))).toBe(true)
+        expect(Array.isArray(shuffleArray(testArr2))).toBe(true)
+        expect(Array.isArray(shuffleArray(testArr3))).toBe(true)
+    })
+
+    test('return an array of the same length as the input', () => {
+        expect(shuffleArray(testArr1)).toHaveLength(testArr1.length)
+        expect(shuffleArray(testArr2)).toHaveLength(testArr2.length)
+        expect(shuffleArray(testArr3)).toHaveLength(testArr3.length)
+    })
 })
