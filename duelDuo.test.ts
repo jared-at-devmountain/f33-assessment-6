1f7a7802539186bf37f9843c45b12489202cd856
Jared Collier <jaredcollier@Jareds-MacBook-Pro.local>
2023-01-13 16:39:01 -0700

    adds automated tests: updates chromedriver

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -6,15 +6,31 @@ require('chromedriver')
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
 beforeEach(async () => {
-    driver.get('http://localhost:3000/')
+    await driver.get('http://localhost:4000/')
 })
 
 afterAll(async () => {
-    driver.quit()
+    await driver.quit()
 })
 
 test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
     const displayed = await title.isDisplayed()
+    expect(displayed).toBe(true)
+})
+
+test('Clicking the Draw button displays the div with id = “choices”', async () => {
+    await driver.findElement(By.id('draw')).click()
+    const choicesSection = await driver.findElement(By.id('choices'))
+    const displayed = await choicesSection.isDisplayed()
+    expect(displayed).toBe(true)
+})
+
+test('Check that clicking an “Add to Duo” button displays the div with id = “player-duo”', async () => {
+    await driver.findElement(By.id('draw')).click()
+    await driver.sleep(100)
+    await driver.findElement(By.css('.bot-btn')).click()
+    const duoSection = await driver.findElement(By.id('player-duo'))
+    const displayed = await duoSection.isDisplayed()
     expect(displayed).toBe(true)
 })
